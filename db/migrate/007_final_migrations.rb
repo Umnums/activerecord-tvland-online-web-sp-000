db966a809abd5c412617c0f772b773e8fe4181a4
Joseph Kao <josephtkao@gmail.com>
2020-06-26 20:12:52 +0000

    Automatically backed up by Learn

--- db/migrate/007_final_migrations.rb
+++ db/migrate/007_final_migrations.rb
@@ -3,6 +3,8 @@ class FinalMigrations < ActiveRecord::Migration[5.1]
   #a new migration, and then in the change method,
   #we make the change.
   def change
-    add_column :characters, :catchphrase, :text
+    add_column :shows, :day, :text
+    add_column :shows, :genre, :text
+    add_column :shows, :season, :text
   end
 end
